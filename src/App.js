281658ae1c1edba1ee022765339905c0f33c86ae
Aditya <adityasingh14102001@gmail.com>
2022-08-15 22:52:03 +0530

    Created sign up functionality and added custom form input + button components

--- src/App.js
+++ src/App.js
@@ -1,62 +1,21 @@
-import CategoryItem from './components/category-item/category-item.component.jsx';
-import Directory from './components/directory/directory.component';
+import Home from './routes/home/home.component.jsx';
+import { Routes, Route } from 'react-router-dom';
+import Navigation from './routes/navigation/navigation.component.jsx';
+import SignIn from './routes/sign-in/sign-in.component.js';
+
+const Shop = () => {
+	return <h1>This is the shop component</h1>;
+};
 
 const App = () => {
-	// const categories = [
-	// 	{
-	// 		id: 1,
-	// 		title: 'Hats',
-	// 	},
-	// 	{
-	// 		id: 2,
-	// 		title: 'Jackets',
-	// 	},
-	// 	{
-	// 		id: 3,
-	// 		title: 'Sneakers',
-	// 	},
-	// 	{
-	// 		id: 4,
-	// 		title: "Women's",
-	// 	},
-	// 	{
-	// 		id: 5,
-	// 		title: "Men's",
-	// 	},
-	// ];
-
-	const categories = [
-		{
-			id: 1,
-			title: 'Hats',
-			imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-		},
-		{
-			id: 2,
-			title: 'Jackets',
-			imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-		},
-		{
-			id: 3,
-			title: 'Sneakers',
-			imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-		},
-		{
-			id: 4,
-			title: 'Womens',
-			imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-		},
-		{
-			id: 5,
-			title: 'Mens',
-			imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-		},
-	];
-
 	return (
-		<div className="categories-container">
-			<Directory categories={categories} />
-		</div>
+		<Routes>
+			<Route path="/" element={<Navigation />}>
+				<Route index element={<Home />} />
+				<Route path="shop" element={<Shop />} />
+				<Route path="sign-in" element={<SignIn />} />
+			</Route>
+		</Routes>
 	);
 };
 
